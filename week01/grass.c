d5ccd6901dc428742cec035712febcab4eed4f5d
Fortran <84888762+Formula-Translation@users.noreply.github.com>
2022-07-03 23:16:50 +0700

    Create grass.c

---
+++ week01/grass.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+  // set type
+  int length,width;
+  double area,ftarea, time; 
+  // Ask for length and width then get and save the number user types
+  printf("length(m.) : ");
+  scanf("%d", &length);
+  printf("width(m.)
+  scanf("%d", &width);
+  
+   //check case
+   if (O < length && 0< width && length <= 1000 && width <= 1000)f
+      //calculation
+      area = length * width*3.28084*3.28084;
+      time = area/2;
+      //ftarea =(area* 3.28084 * 3.28084) / 2.0;
+      printf("InUse %0.5f seconds.", time);
+    }
+}
